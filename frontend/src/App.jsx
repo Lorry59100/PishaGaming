ea2cfd77e80c7aca3d42ac777e3635274cbf1ba8
pisha <pisha@hotmail.fr>
2023-10-25 13:52:52 +0200

    [INIT] - Navbar component first include

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './assets/styles/App.css'
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./routes/Routes"
+import Navbar from './components/layouts/Navbar/Navbar';
 
 function App() {
 
   return (
     <BrowserRouter>
       <div>
+        <Navbar/>
         <main>
           <Routes />
         </main>
