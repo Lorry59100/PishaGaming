bbf3b08eb96f528bee7a58404bd96de5372baf91
pisha <pisha@hotmail.fr>
2023-10-29 11:09:50 +0100

    [INIT] - ProductsController first init

---
+++ backend/src/Controller/ProductsController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductsController extends AbstractController
+{
+    #[Route('/products', name: 'app_products')]
+    public function index(): Response
+    {
+        return $this->render('products/index.html.twig', [
+            'controller_name' => 'ProductsController',
+        ]);
+    }
+
+    /**
+     * @Route("/products-list", name="products_list", methods={"GET"})
+     */
+
+    public function productsList(ProductRepository $productRepository) : JsonResponse
+    {
+        $products = $productRepository->findAll();
+        $productsArray = [];
+        foreach($products as $product) {
+            $categorys = [];
+            foreach($product->getCategory() as $category) {
+                $categorys[] = [
+                    'id' => $category->getId(),
+                    'name' => $category->getName(),
+                ];
+            }
+            $editions = [];
+            foreach($product->getEdition() as $edition) {
+                $editions[] = [
+                    'id' => $category->getId(),
+                    'old_price' => $edition->getOldPrice(),
+                    'price' => $edition->getPrice(),
+                    'stock' => $edition->getStock(),
+                    'img' => $edition->getImg(),
+                    'edition_category' => $edition->getEditionCategory()->getName(),
+                ];
+            }
+            $genres = [];
+            foreach($product->getGenre() as $genre) {
+                $genres[] = [
+                    'id' => $genre->getId(),
+                    'name' => $genre->getName(),
+                ];
+            }
+            $platforms = [];
+            foreach($product->getPlatform() as $platform) {
+                $platforms[] = [
+                    'id' => $platform->getId(),
+                    'name' => $platform->getName(),
+                ];
+            }
+            $tags = [];
+            foreach($product->getTag() as $tag) {
+                $tags[] = [
+                    'id' => $tag->getId(),
+                    'name' => $tag->getName(),
+                ];
+            }
+
+            $productsArray[] = [
+                'id' => $product->getId(),
+                'name' => $product->getName(),
+                'trailer' => $product->getTrailer(),
+                'dev' => $product->getDev(),
+                'editor' => $product->getEditor(),
+                'release' => $product->getRelease(),
+                'description' => $product->getDescription(),
+                'categorys' => $categorys,
+                'editions' => $editions,
+                'genres' => $genres,
+                'platforms' => $platforms,
+                'tags' => $tags,
+            ];
+        }
+        return new JsonResponse($productsArray, 200);
+    }
+}
