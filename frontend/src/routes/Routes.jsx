c68a0d5e14de97b9ec585d744be02fab26d22a0e
pisha <pisha@hotmail.fr>
2023-11-10 23:43:26 +0100

    [INIT] - Import and route to AdminView component

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes as RoutesContainer } from "react-router-dom";
 import Homeview from "../views/Homeview";
 import * as URL from "../constants/urls/URLFront"
+import AdminView from "../views/AdminView";
 
 function Routes() {
   return (
     <RoutesContainer>
         <Route path={URL.URL_HOME} element={<Homeview />} />
+        <Route path={URL.URL_ADMIN} element={<AdminView />} />
     </RoutesContainer>
   )
 }
