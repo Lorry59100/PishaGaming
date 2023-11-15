b30d49fca7001341998226eef6453de52014432d
pisha <pisha@hotmail.fr>
2023-11-15 16:20:14 +0100

    [FEAT] - addProduct first init

--- backend/src/Controller/AdminController.php
+++ backend/src/Controller/AdminController.php
@@ -2,11 +2,17 @@
 
 namespace App\Controller;
 
+use DateTime;
+use App\Entity\Edition;
+use App\Entity\Product;
 use App\Repository\TagRepository;
 use App\Repository\GenreRepository;
+use App\Repository\EditionRepository;
 use App\Repository\CategoryRepository;
 use App\Repository\PlatformRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\EditionCategoryRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -75,5 +81,57 @@ class AdminController extends AbstractController
             'platforms' => $platformArray,
             'tags' => $tagArray,
         ], 200);
+     }
+
+     /**
+     * @Route("/add-product", name="add_product", methods={"POST"})
+     */
+
+     public function addproduct(Request $request, EntityManagerInterface $em, CategoryRepository $categoryRepository, EditionRepository $editionRepository,
+     EditionCategoryRepository $editionCategoryRepository,GenreRepository $genreRepository, PlatformRepository $platformRepository,
+     TagRepository $tagRepository) {
+        $data = json_decode($request->getContent(), true);
+        $product = new Product;
+        $category = $categoryRepository->find($data['category']);
+        
+        $editionCategory = $editionCategoryRepository->find($data['edition']);
+        $edition = new Edition;
+        $edition->setEditionCategory($editionCategory)
+                ->setOldPrice($data['old_price'])
+                ->setPrice($data['price'])
+                ->setStock($data['stock'])
+                ->setImg($data['img']);
+        $em->persist($edition);
+        $em->flush(); 
+
+        $tagIds = $data['tags'];
+        $tags = [];
+        foreach($tagIds as $tagId) {
+        $tag = $tagRepository->find($tagId);
+        if ($tag) {
+        $tags[] = $tag;
+        }
+        }
+
+        foreach ($tags as $tag) {
+        $product->addTag($tag);
+        }
+
+        $genre = $genreRepository->find($data['genre']);
+        $platform = $platformRepository->find($data['platform']);
+        $release = new \DateTime($data['release']);
+        $product->setName($data['name'])
+                ->setTrailer($data['trailer'])
+                ->setDev($data['dev'])
+                ->setDescription($data['description'])
+                ->setEditor($data['editor'])
+                ->setRelease($release)
+                ->addCategory($category)
+                ->addEdition($edition)
+                ->addGenre($genre)
+                ->addPlatform($platform);
+        $em->persist($product);
+        $em->flush();
+        return new JsonResponse($data, 200);
      }
 }
