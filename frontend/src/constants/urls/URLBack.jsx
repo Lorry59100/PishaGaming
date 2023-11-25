0ba11b51c28cf4ddbc9adb08aae7d5ba36b36d46
pisha <pisha@hotmail.fr>
2023-11-25 12:31:23 +0100

    [FIX] - rename URLBack.jsx => UrlBack.js

--- frontend/src/constants/urls/URLBack.jsx
+++
@@ -1,4 +0,0 @@
-export const URL = "https://127.0.0.1:8000";
-export const URL_PRODUCTS_LIST = "/products-list";
-export const URL_SUB_LISTS = "/sub-lists";
-export const URL_ADD_PRODUCT = '/add-product';
