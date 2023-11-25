f093923cb7f38a45f165fe15832ddde319586140
pisha <pisha@hotmail.fr>
2023-11-25 12:35:33 +0100

    [FIX] - axios request fixed

--- frontend/src/components/products/GameBanner.jsx
+++ frontend/src/components/products/GameBanner.jsx
@@ -9,58 +9,45 @@ function GameBanner() {
 
     useEffect(() => {
         axios.get(`${URL}${URL_PRODUCTS_LIST}`)
-        .then(response => {
-            console.log(response.data);
-            // Filtrer les produits par catégorie et édition
-            const filteredProducts = response.data.map(product => {
-                const hasJeuxCategory = product.categorys.some(category => category.id === 1);
-                const standardEdition = product.editions.find(edition => edition.edition_category === 'Standart');
-                // Vérifier s'il y a une édition Standart Edition
-                if (hasJeuxCategory && standardEdition) {
-                    return {
-                        ...product,
-                        editions: [standardEdition]
-                    };
-                }
-                return null; // Retourner null pour les produits sans édition Standart Edition
-            }).filter(product => product !== null); // Filtrer les produits non null
-    
-            // Choisir un produit au hasard parmi les produits filtrés
-            const randomIndex = Math.floor(Math.random() * filteredProducts.length);
-            const randomProduct = filteredProducts[randomIndex];
-            console.log(randomProduct);
-            setRandomProduct(randomProduct);
-        })
-        .catch(error => {
-            console.error('Erreur lors de la récupération de la liste de jeux :', error)
-        });
+            .then(response => {
+                console.log(response.data);
+                // Récupérer la liste des produits directement
+                const productList = response.data;
+                console.log(productList);
+                // Mettre à jour l'état avec la liste des produits
+                setRandomProduct(productList);
+            })
+            .catch(error => {
+                console.error('Erreur lors de la récupération de la liste de jeux :', error)
+            });
     }, []);
     
 
     return (
         <div className="game-banner-container">
             <div className="background-header-container">
-            {randomProduct && (
+                {randomProduct && (
                     <div className='random-game-container'>
-                        {randomProduct.editions.map(edition => (
-                            <div key={edition.id}>
-                                <div className="background-header" style={{ backgroundImage: `url(${edition.img})` }}></div>
+                        {randomProduct.map(product => (
+                            <div key={product.id}>
+                                <div className="background-header" style={{ backgroundImage: `url(${product.img})` }}></div>
                                 <div className="random-game-content">
-                                    <h1>{randomProduct.name}</h1>
+                                    <h1>{product.name}</h1>
                                     <div className="random-game-price">
                                         <div className="discount-label"> 
-                                            <h4><strong>-</strong>{calculateDiscountPercentage(edition.old_price, edition.price)}</h4>
+                                            <h4><strong>-</strong>{calculateDiscountPercentage(product.old_price, product.price)}</h4>
                                         </div>
-                                        <h1>{convertToEuros(edition.price)} €</h1>
+                                        <h1>{convertToEuros(product.price)} €</h1>
                                     </div>
                                 </div>
-                            </div>   
+                            </div>
                         ))}
                     </div>
                 )}
             </div>
         </div>
     );
+    
 }
 
 export default GameBanner;
