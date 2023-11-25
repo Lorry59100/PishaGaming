e765a94f0ed8a42f6c3edd906277eae54636c6a7
pisha <pisha@hotmail.fr>
2023-11-25 12:34:48 +0100

    [FIX] - supress of Admin front logic

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -1,13 +1,11 @@
 import { Route, Routes as RoutesContainer } from "react-router-dom";
 import Homeview from "../views/Homeview";
 import * as URL from "../constants/urls/URLFront"
-import AdminView from "../views/AdminView";
 
 function Routes() {
   return (
     <RoutesContainer>
         <Route path={URL.URL_HOME} element={<Homeview />} />
-        <Route path={URL.URL_ADMIN} element={<AdminView />} />
     </RoutesContainer>
   )
 }
