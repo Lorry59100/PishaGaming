f0311aef02735035334c08e7b98b04a027981d13
pisha <pisha@hotmail.fr>
2023-11-26 13:27:34 +0100

    [FIX] - axios request fixed

--- frontend/src/components/products/GameBanner.jsx
+++ frontend/src/components/products/GameBanner.jsx
@@ -9,39 +9,39 @@ function GameBanner() {
 
     useEffect(() => {
         axios.get(`${URL}${URL_PRODUCTS_LIST}`)
-            .then(response => {
-                console.log(response.data);
-                // Récupérer la liste des produits directement
-                const productList = response.data;
-                console.log(productList);
-                // Mettre à jour l'état avec la liste des produits
-                setRandomProduct(productList);
+        .then(response => {
+            const productList = response.data;
+            const filteredProducts = productList.filter(product => {
+                const hasCategory = product.category && product.category.name === "Jeux vidéo";
+                const hasEdition = product.edition && product.edition.name === "Standart";
+                return hasCategory && hasEdition;
+            });
+                    const randomIndex = Math.floor(Math.random() * filteredProducts.length);
+                    const randomProduct = filteredProducts[randomIndex];
+                    setRandomProduct(randomProduct);
             })
             .catch(error => {
                 console.error('Erreur lors de la récupération de la liste de jeux :', error)
             });
     }, []);
-    
 
     return (
         <div className="game-banner-container">
             <div className="background-header-container">
                 {randomProduct && (
                     <div className='random-game-container'>
-                        {randomProduct.map(product => (
-                            <div key={product.id}>
-                                <div className="background-header" style={{ backgroundImage: `url(${product.img})` }}></div>
-                                <div className="random-game-content">
-                                    <h1>{product.name}</h1>
-                                    <div className="random-game-price">
-                                        <div className="discount-label"> 
-                                            <h4><strong>-</strong>{calculateDiscountPercentage(product.old_price, product.price)}</h4>
-                                        </div>
-                                        <h1>{convertToEuros(product.price)} €</h1>
+                        <div key={randomProduct.id}>
+                            <div className="background-header" style={{ backgroundImage: `url(${randomProduct.img})` }}></div>
+                            <div className="random-game-content">
+                                <h1>{randomProduct.name}</h1>
+                                <div className="random-game-price">
+                                    <div className="discount-label"> 
+                                        <h4><strong>-</strong>{calculateDiscountPercentage(randomProduct.old_price, randomProduct.price)}</h4>
                                     </div>
+                                    <h1>{convertToEuros(randomProduct.price)} €</h1>
                                 </div>
                             </div>
-                        ))}
+                        </div>
                     </div>
                 )}
             </div>
