01f9fc217afcf50b5a2e43dc07fbacc00d459d93
pisha <pisha@hotmail.fr>
2023-11-27 15:48:09 +0100

    [INIT] - axios request and formik init

--- frontend/src/components/account/forms/RegisterForm.jsx
+++ frontend/src/components/account/forms/RegisterForm.jsx
@@ -1,33 +1,63 @@
 import { useState  } from "react";
+import axios from "axios";
 import { Formik, Form, Field } from 'formik';
-import DatePicker from "react-datepicker";
+import { URL, URL_REGISTER } from "../../../constants/urls/URLBack";
 import "react-datepicker/dist/react-datepicker.css";
-import {fr} from "date-fns/locale";
+import DatePicker from "react-datepicker";
+import { registerLocale } from "react-datepicker";
+import fr from "date-fns/locale/fr";
+registerLocale("fr", fr);
 export function RegisterForm() {
-    const [startDate, setStartDate] = useState(null);
+  const [birthDate, setBirthDate] = useState(null);
+    // Logique de controle du formulaire
+    const initialValues= {
+      firstname: '',
+      lastname: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      };
+
+    const onSubmit=(values) => {
+      console.log(values);
+      axios.post(`${URL}${URL_REGISTER}`, {
+        firstname: values.firstname,
+        lastname: values.lastname,
+        email: values.email,
+        password: values.password,
+        birthDate: birthDate,
+      })
+      .then((response) => {
+        console.log('Response data', response.data);
+      })
+      .catch((error) => {
+        console.error('Erreur lors de la récupération des données :', error);
+      });
+    };
+
   return (
     <div className="little-form-container">
-            <Formik initialValues={{}} onSubmit={""}>
+            <Formik initialValues={initialValues} onSubmit={onSubmit}>
+              {({ setFieldValue }) => (
               <Form>
                 <div className="register-fields-container">
                   <Field type="text" name="firstname" placeholder="Prénom"/>
                   <Field type="text" name="lastname" placeholder="Nom"/>
                   <Field type="password" name="password" placeholder="Votre mot de passe"/>
-                  <Field type="password" name="password" placeholder="Confirmez votre mot de passe"/>
+                  <Field type="password" name="confirmPassword" placeholder="Confirmez votre mot de passe"/>
                   <Field type="email" name="email" placeholder="Email"/>
                   <div className="datepicker-container">
-              <DatePicker 
-              className="datepicker"
-              placeholderText="Date de naissance"
-              selected={startDate} 
-              onChange={(date) => setStartDate(date)}
-              locale={fr}
-              />
-            </div>
+                  <DatePicker dateFormat="dd/MM/yyyy" placeholderText="Date de naissance" selected={birthDate} onChange={(date) => {
+                    setFieldValue('birthDate', date); // Met à jour la valeur du champ
+                    setBirthDate(date); // Met à jour l'état local
+                  }}/>
+                  </div>
                 </div>
-                <div className="legal">
-                <input type="checkbox" name="legal" id="" />
-                J'accepte les conditions de ventes et la politique de confidentialité
+                <div className="consent">
+                  <input type="checkbox" name="consent" id="" />
+                  <p className="preserve-space">
+                    J'accepte <a href="">les conditions de ventes</a> et <a href="">la politique de confidentialité</a>
+                  </p>
                 </div>
                 <div className="btn-container">
                   <button className="submit-button middle-column" type="submit">
@@ -35,6 +65,7 @@ export function RegisterForm() {
                   </button>
                 </div>
               </Form> 
+              )}
             </Formik>
             <div className="back">
               <button>&lt;&lt; Retour</button>
