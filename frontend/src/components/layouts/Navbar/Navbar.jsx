9dc45dc958937c4003d62655f07497f67a3aa88b
pisha <pisha@hotmail.fr>
2023-11-28 12:56:30 +0100

    [INIT] - refactoring with tokenService & Link to Admin Interface

--- frontend/src/components/layouts/Navbar/Navbar.jsx
+++ frontend/src/components/layouts/Navbar/Navbar.jsx
@@ -1,13 +1,17 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 import { TiShoppingCart } from 'react-icons/ti';
 import { PiUserBold } from 'react-icons/pi';
-import { IconContext } from "react-icons";
-import logo from "../../../assets/img/Logo.png"
+import { IconContext } from 'react-icons';
+import logo from '../../../assets/img/Logo.png';
 import Searchbar from './Searchbar';
-import "../../../assets/styles/components/navbar.css"
+import '../../../assets/styles/components/navbar.css';
+import { useAuth } from '../../account/services/tokenService';
+import { URL, URL_ADMIN } from '../../../constants/urls/URLBack';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
-const [isProfileVisible, setIsProfileVisible] = useState(false);
+  const { userToken, decodedUserToken, logout } = useAuth();
+  const [isProfileVisible, setIsProfileVisible] = useState(false);
   const [scrolled, setScrolled] = useState(false);
   const [isVisible, setIsVisible] = useState(true);
   const [isAtTop, setIsAtTop] = useState(true);
@@ -17,37 +21,39 @@ const [isProfileVisible, setIsProfileVisible] = useState(false);
     setIsProfileVisible(!isProfileVisible);
   };
 
-    useEffect(() => {
-      const handleScroll = () => {
-        const isScrolled = window.scrollY > 0;
-        if (isScrolled !== scrolled) {
-          setScrolled(isScrolled);
-        }
-  
-        const shouldShowLinks = window.scrollY === 0;
-        if (shouldShowLinks !== isVisible) {
-          setIsVisible(shouldShowLinks);
-        }
-  
-        const atTop = window.scrollY === 0;
-        if (atTop !== isAtTop) {
-          setIsAtTop(atTop);
-          // Ajoute ou retire la classe 'center-menu' en fonction de la position de défilement
-          setMenuClass(atTop ? '' : 'center-menu');
-        }
-      };
-  
-      window.addEventListener('scroll', handleScroll);
-  
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, [scrolled, isVisible, isAtTop]);
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 0;
+      if (isScrolled !== scrolled) {
+        setScrolled(isScrolled);
+      }
+
+      const shouldShowLinks = window.scrollY === 0;
+      if (shouldShowLinks !== isVisible) {
+        setIsVisible(shouldShowLinks);
+      }
+
+      const atTop = window.scrollY === 0;
+      if (atTop !== isAtTop) {
+        setIsAtTop(atTop);
+        setMenuClass(atTop ? '' : 'center-menu');
+      }
+    };
+
+    handleScroll();
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [isVisible, isAtTop, userToken, scrolled]);
 
   return (
     <div className={`navbar ${scrolled ? 'scrolled' : ''} ${isVisible ? 'visible' : 'hidden'} ${isAtTop ? 'at-top' : ''}`}>
+      
       <div className="logo">
-        <a href=""><img src={logo} alt="logo" className='orange-logo'/></a>
+        <a href=""><img src={logo} alt="logo" className="orange-logo" /></a>
         <h3>Pisha Gaming</h3>
       </div>
       <div className={`menu ${menuClass}`}>
@@ -57,19 +63,17 @@ const [isProfileVisible, setIsProfileVisible] = useState(false);
           <a href="/">Prochaines sorties</a>
           <a href="/">Support 24/7</a>
         </div>
-        <Searchbar/>
+        <Searchbar />
       </div>
 
       {/* Icônes droite de la navbar */}
       <div className="cart-profile">
-          <IconContext.Provider value={{ size: "2em" }}>
+        <IconContext.Provider value={{ size: '2em' }}>
           <a href="/"><TiShoppingCart /></a>
-
           <button onClick={toggleProfileVisibility}><PiUserBold /></button>
+        </IconContext.Provider>
 
-          </IconContext.Provider>
-
-          {isProfileVisible && (
+        {isProfileVisible && (
           <div className="profile-content">
             <ul>
               <li>
@@ -81,15 +85,22 @@ const [isProfileVisible, setIsProfileVisible] = useState(false);
               <li>
                 <a href="/">Wishlist</a>
               </li>
-              <li>
-                <a href="/">Déconnexion</a>
-              </li>
+              {decodedUserToken && (
+              <div>
+                <li>
+                  <a href="/" onClick={logout}>Déconnexion</a>
+                </li>
+                <li>
+                <Link to={`${URL}${URL_ADMIN}`}>Admin</Link>
+                </li>
+              </div>
+              )}
             </ul>
           </div>
-          )}
+        )}
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
