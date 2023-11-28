89042c6521a7802086415ff5b10544f5b9aecaee
pisha <pisha@hotmail.fr>
2023-11-28 10:59:46 +0100

    [INIT] - JWT create on login

--- backend/src/Controller/LoginController.php
+++ backend/src/Controller/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,7 +16,7 @@ class LoginController extends AbstractController
     /**
      * @Route("/login", name="login", methods={"POST"})
      */
-    public function login(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher)
+    public function login(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher, JWTTokenManagerInterface $jwtManager)
     {
         $data = json_decode($request->getContent(), true);
 
@@ -25,6 +26,17 @@ class LoginController extends AbstractController
         $user = $userRepository->findOneBy(['email' => $email]);
 
         if ($user && $passwordHasher->isPasswordValid($user, $password)) {
+            $payload = [
+                'firstname' => $user->getFirstname(),
+                'lastname' => $user->getLastname(),
+                'role' => $user->getRoles(),
+                'id' => $user->getId(),
+            ];
+            $token = $jwtManager->create($user, $payload);
+            return $this->json([
+                'user'  => $user->getUserIdentifier(),
+                'token' => $token,
+            ]);
             // Utilisateur authentifié
             return new JsonResponse(['message' => 'Connexion réussie'], 200);
         }
