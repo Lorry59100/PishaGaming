6bebb644256e954ec6221720299a7ed5321f8bda
pisha <pisha@hotmail.fr>
2023-11-29 19:02:24 +0100

    [FIX] - admin url const changed

--- frontend/src/constants/urls/URLBack.js
+++ frontend/src/constants/urls/URLBack.js
@@ -1,5 +1,5 @@
 export const URL = "https://127.0.0.1:8000";
-export const URL_ADMIN = "/admin"
+export const URL_ADMIN = "/logadmin"
 export const URL_REGISTER = "/register";
 export const URL_LOGIN = "/login";
 export const URL_PRODUCTS_LIST = "/products-list";
