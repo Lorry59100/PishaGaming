120aaaa7159281320774f620632c433ef9bac167
pisha <pisha@hotmail.fr>
2023-11-29 21:11:34 +0100

    [INIT] - Form state first init

--- frontend/src/components/account/forms/LoginAndRegisterForm.jsx
+++ frontend/src/components/account/forms/LoginAndRegisterForm.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import { ImCross } from "react-icons/im";
 import { IconContext } from "react-icons";
 import logo from "../../../assets/img/Logo.png";
 import "../../../assets/styles/components/form.css";
 import "react-datepicker/dist/react-datepicker.css";
+import PropTypes from "prop-types";
 
 import { LoginForm } from "./LoginForm";
 import { RegisterForm } from "./RegisterForm";
 
-export function LoginAndRegisterForm() {
+export function LoginAndRegisterForm({onCloseForm}) {
+  const [showLoginForm, setShowLoginForm] = useState(true);
+
+  const toggleForm = () => {
+    setShowLoginForm(!showLoginForm);
+  };
+
+  const handleCloseForm = () => {
+    setShowLoginForm(false);
+    onCloseForm(); // Notify Navbar.jsx about form closure
+  };
   return (
       <div className="add-product-panel">
         <div className="form-container">
@@ -17,14 +29,17 @@ export function LoginAndRegisterForm() {
             <h3>Pisha Gaming</h3>
             </a>
           </div>
-          <LoginForm/>
-          {/* <RegisterForm/> */}
+          {showLoginForm ? <LoginForm toggleForm={toggleForm} /> : <RegisterForm toggleForm={toggleForm} />}
         </div>
         <div className="wallpaper-container">
         <IconContext.Provider value={{ size: "1.5em" }}>
-        <ImCross onClick={""}/>
+          <ImCross onClick={handleCloseForm} />
         </IconContext.Provider>
         </div>
       </div>
 );
 }
+
+LoginAndRegisterForm.propTypes = {
+  onCloseForm: PropTypes.func.isRequired,
+};
