4780be489301dd9920eb65c2842788b504ea0201
pisha <pisha@hotmail.fr>
2023-11-30 16:16:01 +0100

    [INIT] - SingleProduct Route first init

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes as RoutesContainer } from "react-router-dom";
 import Homeview from "../views/Homeview";
 import * as URL from "../constants/urls/URLFront"
+import { SingleProductview } from "../views/SingleProductview";
 
 function Routes() {
   return (
     <RoutesContainer>
         <Route path={URL.URL_HOME} element={<Homeview />} />
+        <Route path={URL.URL_SINGLE_PRODUCT} element={<SingleProductview />} />
     </RoutesContainer>
   )
 }
