41080799d9d5087634707a7d20a014b4eff1f4eb
pisha <pisha@hotmail.fr>
2023-12-03 19:57:03 +0100

    [INIT] - add items from storage to Db on connect

--- backend/src/Controller/LoginController.php
+++ backend/src/Controller/LoginController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Cart;
+use App\Entity\User;
+use App\Repository\ProductRepository;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -15,12 +19,14 @@ class LoginController extends AbstractController
     /**
      * @Route("/login", name="login", methods={"POST"})
      */
-    public function login(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher, JWTTokenManagerInterface $jwtManager)
+    public function login(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher, JWTTokenManagerInterface $jwtManager, 
+    ProductRepository $productRepository, EntityManagerInterface $entityManager)
     {
         $data = json_decode($request->getContent(), true);
 
         $email = $data['email'];
         $password = $data['password'];
+        $cart = $data['cart'] ?? []; // Retrieve the cart information
 
         $user = $userRepository->findOneBy(['email' => $email]);
 
@@ -31,6 +37,10 @@ class LoginController extends AbstractController
                 'id' => $user->getId(),
             ];
             $token = $jwtManager->createFromPayload($user, $payload);
+            // Check if the user has a cart
+            if ($cart) {
+            $this->createCartEntities($user, $productRepository ,$cart, $entityManager);
+            }
  
             return new JsonResponse([
                 'user'  => $user->getUserIdentifier(),
@@ -39,5 +49,34 @@ class LoginController extends AbstractController
         }
         return new JsonResponse(['message' => 'Identifiants invalides'], 401);
        
+    }
+
+    private function createCartEntities(User $user, ProductRepository $productRepository, array $cartItems, EntityManagerInterface $entityManager)
+    {
+        foreach ($cartItems as $cartItem) {
+            $product = $productRepository->findOneBy(['id' => $cartItem['id']]);
+            $existingCartItem = $entityManager->getRepository(Cart::class)->findOneBy([
+                'user' => $user,
+                'product' => $product,
+                'platform' => $cartItem['platform'],
+        ]);
+
+        if ($existingCartItem) {
+            // If the product is already in the cart, update quantity and price
+            $existingCartItem->setQuantity($existingCartItem->getQuantity() + $cartItem['quantity']);
+        } else {
+            // If the product is not in the cart, create a new cart entity
+            $cartEntity = new Cart();
+            $cartEntity
+            ->setUser($user)
+            ->setProduct($product)
+            ->setQuantity($cartItem['quantity'])
+            ->setPlatform($cartItem['platform']);
+            // Add the Cart entity to the EntityManager
+            $entityManager->persist($cartEntity);
+            }
+        }
+        // Flush all changes to the database
+        $entityManager->flush();
     }
 }
