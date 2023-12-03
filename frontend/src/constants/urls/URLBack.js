9a57f690230c7f76f0cd5ec4df9e1eacbee41fd2
pisha <pisha@hotmail.fr>
2023-12-03 20:04:24 +0100

    [INIT] - URL back for add-to-cart and get-cart first init

--- frontend/src/constants/urls/URLBack.js
+++ frontend/src/constants/urls/URLBack.js
@@ -4,3 +4,5 @@ export const URL_REGISTER = "/register";
 export const URL_LOGIN = "/login";
 export const URL_PRODUCTS_LIST = "/products-list";
 export const URL_SINGLE_PRODUCT = "/single-product"
+export const URL_ADD_TO_CART = "/add-to-cart"
+export const URL_USER_CART = '/get-cart'
