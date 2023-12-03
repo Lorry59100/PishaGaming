54486c018f266c9387ebba25c8469218499d22e3
pisha <pisha@hotmail.fr>
2023-12-03 20:06:07 +0100

    [INIT] - CartView path added to Route

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -2,12 +2,14 @@ import { Route, Routes as RoutesContainer } from "react-router-dom";
 import Homeview from "../views/Homeview";
 import * as URL from "../constants/urls/URLFront"
 import { SingleProductview } from "../views/SingleProductview";
+import { Cartview } from "../views/Cartview";
 
 function Routes() {
   return (
     <RoutesContainer>
         <Route path={URL.URL_HOME} element={<Homeview />} />
         <Route path={URL.URL_SINGLE_PRODUCT} element={<SingleProductview />} />
+        <Route path={URL.URL_CART} element={<Cartview />} />
     </RoutesContainer>
   )
 }
