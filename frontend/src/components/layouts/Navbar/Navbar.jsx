1115d4dab3b9c38cfbeff400225004e417b8a257
pisha <pisha@hotmail.fr>
2023-12-05 12:30:09 +0100

    [INIT] - context for Navbar component visibility first init

--- frontend/src/components/layouts/Navbar/Navbar.jsx
+++ frontend/src/components/layouts/Navbar/Navbar.jsx
@@ -9,6 +9,8 @@ import { useAuth } from '../../account/services/tokenService';
 import { URL, URL_ADMIN } from '../../../constants/urls/URLBack';
 import { Link } from 'react-router-dom';
 import { LoginAndRegisterForm } from '../../account/forms/LoginAndRegisterForm';
+import { NavbarVisibilityContext } from '../../../contexts/NavbarVisibilityContext';
+import { useContext } from 'react';
 
 function Navbar() {
   const { userToken, decodedUserToken, logout, login } = useAuth();
@@ -18,6 +20,7 @@ function Navbar() {
   const [isAtTop, setIsAtTop] = useState(true);
   const [menuClass, setMenuClass] = useState('');
   const [showLoginAndRegisterForm, setShowLoginAndRegisterForm] = useState(false);
+  const { isNavbarVisible } = useContext(NavbarVisibilityContext);
 
   const toggleProfileVisibility = () => {
     if (!userToken) {
@@ -56,69 +59,71 @@ function Navbar() {
   }, [isVisible, isAtTop, userToken, scrolled]);
 
   return (
-    <div className={`navbar ${showLoginAndRegisterForm ? '' : (scrolled ? 'scrolled' : '')} ${isVisible ? 'visible' : 'hidden'} ${isAtTop ? 'at-top' : ''}`}>
-      
-      <div className="logo">
-        <a href=""><img src={logo} alt="logo" className="orange-logo" /></a>
-        <h3>Pisha Gaming</h3>
-      </div>
-      <div className={`menu ${menuClass}`}>
-        <div className="links">
-          <a href="/">Tendances</a>
-          <a href="/">Précommandes</a>
-          <a href="/">Prochaines sorties</a>
-          <a href="/">Support 24/7</a>
+    // Utilisation de la condition isNavbarVisible pour rendre la Navbar visible ou non
+    isNavbarVisible && (
+      <div className={`navbar ${showLoginAndRegisterForm ? '' : (scrolled ? 'scrolled' : '')} ${isVisible ? 'visible' : 'hidden'} ${isAtTop ? 'at-top' : ''}`}>
+        
+        <div className="logo">
+          <a href=""><img src={logo} alt="logo" className="orange-logo" /></a>
+          <h3>Pisha Gaming</h3>
         </div>
-        <Searchbar />
-      </div>
-      
-    <div className='profile-container'>
-      <div className="cart-profile">
-        <IconContext.Provider value={{ size: '2em' }}>
-          <a href="/"><TiShoppingCart /></a>
-          {!decodedUserToken && (
-            <button onClick={toggleProfileVisibility}><PiUserBold /></button>
-          )}
-          {decodedUserToken && (
-            <button onClick={toggleProfileVisibility}>
-              <IconContext.Provider value={{ size: '2em'}}>
-                <PiUserBold className='user-icon-circled'/>
-              </IconContext.Provider>
-            </button>
-          )}
-        </IconContext.Provider>
-        {userToken && isProfileVisible && (
-          <div className="profile-content">
-            <ul>
-              <li>
-                <a href="/">Paramètres</a>
-              </li>
-              <li>
-                <a href="/">Achats</a>
-              </li>
-              <li>
-                <a href="/">Wishlist</a>
-              </li>
-              {decodedUserToken && (
-              <div className='user-panel-connected'>
-                <li>
-                  <a href="/" onClick={logout}>Déconnexion</a>
-                </li>
-                {decodedUserToken.roles.includes('ROLE_ADMIN') && (
-                    <li>
-                      <Link to={`${URL}${URL_ADMIN}`}>Admin</Link>
-                    </li>
-                  )}
-              </div>
-              )}
-            </ul>
+        <div className={`menu ${menuClass}`}>
+          <div className="links">
+            <a href="/">Tendances</a>
+            <a href="/">Précommandes</a>
+            <a href="/">Prochaines sorties</a>
+            <a href="/">Support 24/7</a>
           </div>
-        )}
-      {!userToken && showLoginAndRegisterForm && <LoginAndRegisterForm login={login} onCloseForm={() => setShowLoginAndRegisterForm(false)} />}
+          <Searchbar />
+        </div>
+        
+        <div className='profile-container'>
+          <div className="cart-profile">
+            <IconContext.Provider value={{ size: '2em' }}>
+              <a href="/"><TiShoppingCart /></a>
+              {!decodedUserToken && (
+                <button onClick={toggleProfileVisibility}><PiUserBold /></button>
+              )}
+              {decodedUserToken && (
+                <button onClick={toggleProfileVisibility}>
+                  <IconContext.Provider value={{ size: '2em'}}>
+                    <PiUserBold className='user-icon-circled'/>
+                  </IconContext.Provider>
+                </button>
+              )}
+            </IconContext.Provider>
+            {userToken && isProfileVisible && (
+              <div className="profile-content">
+                <ul>
+                  <li>
+                    <a href="/">Paramètres</a>
+                  </li>
+                  <li>
+                    <a href="/">Achats</a>
+                  </li>
+                  <li>
+                    <a href="/">Wishlist</a>
+                  </li>
+                  {decodedUserToken && (
+                    <div className='user-panel-connected'>
+                      <li>
+                        <a href="/" onClick={logout}>Déconnexion</a>
+                      </li>
+                      {decodedUserToken.roles.includes('ROLE_ADMIN') && (
+                        <li>
+                          <Link to={`${URL}${URL_ADMIN}`}>Admin</Link>
+                        </li>
+                      )}
+                    </div>
+                  )}
+                </ul>
+              </div>
+            )}
+            {!userToken && showLoginAndRegisterForm && <LoginAndRegisterForm login={login} onCloseForm={() => setShowLoginAndRegisterForm(false)} />}
+          </div>
+        </div>
       </div>
-      
-      </div>
-    </div>
+    )
   );
 }
 
