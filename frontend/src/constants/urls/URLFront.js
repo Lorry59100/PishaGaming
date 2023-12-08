978a84f375eedff69a4bfe1156c3359181aab30f
pisha <pisha@hotmail.fr>
2023-12-08 21:35:10 +0100

    [INIT] URLFront for activation first init

--- frontend/src/constants/urls/URLFront.js
+++ frontend/src/constants/urls/URLFront.js
@@ -2,3 +2,4 @@ export const URL_HOME = '/';
 export const URL_SINGLE_PRODUCT = '/single-product/:id';
 export const URL_CART = '/cart';
 export const URL_PAYMENT = '/payment';
+export const URL_ACTIVATION = '/activation';
