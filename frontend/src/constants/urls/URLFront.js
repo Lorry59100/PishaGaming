9139d2392742e53da5c6c324ef688f4456d42887
pisha <pisha@hotmail.fr>
2023-12-14 17:54:35 +0100

    [INIT] - parameters front URl first init

--- frontend/src/constants/urls/URLFront.js
+++ frontend/src/constants/urls/URLFront.js
@@ -3,3 +3,4 @@ export const URL_SINGLE_PRODUCT = '/single-product/:id';
 export const URL_CART = '/cart';
 export const URL_PAYMENT = '/payment';
 export const URL_ACTIVATION = '/activation';
+export const URL_PARAMETERS = '/parameters';
