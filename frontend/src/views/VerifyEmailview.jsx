ff33f79deabda8304612ac37f92c4ea47d4138ce
pisha <pisha@hotmail.fr>
2023-12-19 12:34:34 +0100

    [INIT] - request to checck account token first init

--- frontend/src/views/VerifyEmailview.jsx
+++ frontend/src/views/VerifyEmailview.jsx
@@ -1,36 +1,53 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { URL, URL_ACCOUNT_ACTIVATION } from "../constants/urls/URLBack";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { ToastErrorWithLink } from "../components/services/toastService";
+import "react-toastify/dist/ReactToastify.css";
+import "../assets/styles/components/verify-account.css"
+import loader from "../assets/img/loader.gif"
+import { URL_HOME } from "../constants/urls/URLFront";
 
 export function VerifyEmailview() {
     const flag = useRef(false);
     const { token } = useParams();
+    const isToastDisplayed = useRef(false);
+    const navigate = useNavigate();
     console.log(token);
 
     useEffect(() => {
-        const handleCheckEmail = async () => {
-          if (flag.current === false) {
-            try {
-              const response = await axios.post(`${URL}${URL_ACCOUNT_ACTIVATION}`, {token: token});
-              console.log('Response data', response.data);
-              if (response.status === 200) {
-                console.log(response);
-              }
-            } catch (error) {
-              console.error('Erreur lors de la récupération des données :', error);
-            } finally {
-              flag.current = true;
+      const handleCheckEmail = async () => {
+        try {
+          const response = await axios.post(`${URL}${URL_ACCOUNT_ACTIVATION}`, { token: token });
+          console.log('Response data', response.data);
+  
+          if (!isToastDisplayed.current) {
+            if (response.status === 200 && response.data.message) {
+              // Traitement réussi
+              console.log(response.data.message);
+            } else if (response.data.error) {
+              ToastErrorWithLink(response.data.error, "en cliquant ici", "http://localhost:5173/resend-activation-token");
+              isToastDisplayed.current = true; // Marquer le toast comme déjà affiché
+              navigate(URL_HOME)
+            } else {
+              // Autres cas non traités
+              console.error('Réponse inattendue du serveur', response);
             }
           }
-        };
-    
-        handleCheckEmail();
-      }, [token]);
+        } catch (error) {
+          // Erreur réseau ou autre
+          ToastError('Une erreur s\'est produite lors de la récupération des données');
+          console.error('Erreur lors de la récupération des données :', error);
+        }
+      };
+  
+      handleCheckEmail();
+    }, [token]);
 
   return (
-    <div>
-      <h3>Verification en cours...</h3>
+    <div className="verify-account-container">
+      <h2>Verification en cours...</h2>
+      <img src={loader} alt="loader"/>
     </div>
   )
 }
