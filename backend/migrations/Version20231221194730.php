6ca18288e139ffd20e6bfc811b38e1d369edbfb9
pisha <pisha@hotmail.fr>
2023-12-21 23:42:39 +0100

    [FIX] - migrations

--- backend/migrations/Version20231221194730.php
+++ backend/migrations/Version20231221194730.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231221132407 extends AbstractMigration
+final class Version20231221194730 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20231221132407 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD mail_to_update VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD token_mail VARCHAR(255) DEFAULT NULL, ADD mail_expiration DATETIME DEFAULT NULL, ADD mail_to_update VARCHAR(255) DEFAULT NULL, CHANGE tokenexpiration token_expiration DATETIME DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user DROP mail_to_update');
+        $this->addSql('ALTER TABLE user ADD tokenexpiration DATETIME DEFAULT NULL, DROP token_expiration, DROP token_mail, DROP mail_expiration, DROP mail_to_update');
     }
 }
