917ef746dc00a0f36bdd6a3f582e48e130c68860
pisha <pisha@hotmail.fr>
2023-12-28 13:28:14 +0100

    [INIT] - URLFront to single-order-historic first init

--- frontend/src/constants/urls/URLFront.js
+++ frontend/src/constants/urls/URLFront.js
@@ -9,3 +9,4 @@ export const URL_CHANGE_EMAIL = '/changeEmail/:token'
 export const URL_RESEND_ACTIVATION_TOKEN = '/resend-activation-token'
 export const URL_FORGOTTEN_PASSWORD = '/forgotten-password'
 export const URL_ORDER_HISTORIC = '/order-historic'
+export const URL_SINGLE_ORDER_HISTORIC = '/single-order-historic';
