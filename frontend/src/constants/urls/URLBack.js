c9c3a2e2b45a741dcf9cd303130e44c0ffcbd2b9
pisha <pisha@hotmail.fr>
2024-06-02 11:38:05 +0200

    [INIT] - URLBack genres-list first init

--- frontend/src/constants/urls/URLBack.js
+++ frontend/src/constants/urls/URLBack.js
@@ -2,6 +2,7 @@ export const URL = "https://127.0.0.1:8000";
 export const URL_ADMIN = "/logadmin"
 export const URL_REGISTER = "/register";
 export const URL_LOGIN = "/login";
+export const URL_GENRES_LIST = "/genres-list";
 export const URL_PRODUCTS_LIST = "/products-list";
 export const URL_SINGLE_PRODUCT = "/single-product"
 export const URL_ADD_TO_CART = "/add-to-cart"
