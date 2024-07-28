90a604f9f0a5fb8dfb898955272bd8776d1b8adc
pisha <pisha@hotmail.fr>
2024-07-28 13:12:46 +0200

    [INIT] - SearchView URL first include

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -5,13 +5,14 @@ import { SingleProductview } from "../views/SingleProductview";
 import { Cartview } from "../views/Cartview";
 import { Paymentview } from "../views/Paymentview";
 import { Activationview } from "../views/Activationview";
-import { Parameterview } from "../views/Parameterview";
+/* import { Parameterview } from "../views/Parameterview"; */
 import { VerifyEmailview } from "../views/VerifyEmailview";
 import { ResendActivationTokenview } from "../views/ResendActivationTokenview";
 import { ChangeMailview } from "../views/ChangeMailview";
 import { ForgottenPasswordview } from "../views/ForgottenPasswordview";
-import OrderHistoricview from "../views/OrderHistoricview";
+/* import OrderHistoricview from "../views/OrderHistoricview"; */
 import SingleOrderHistoricview from "../views/SingleOrderHistoricview";
+import { Searchview } from "../views/Searchview";
 
 function Routes() {
   return (
@@ -21,12 +22,13 @@ function Routes() {
         <Route path={URL.URL_CART} element={<Cartview />} />
         <Route path={URL.URL_PAYMENT} element={<Paymentview />} />
         <Route path={URL.URL_ACTIVATION} element={<Activationview />} />
-        <Route path={URL.URL_PARAMETERS} element={<Parameterview />} />
+        {/* <Route path={URL.URL_PARAMETERS} element={<Parameterview />} /> */}
         <Route path={URL.URL_VERIFY_EMAIL} element={<VerifyEmailview />} />
         <Route path={URL.URL_CHANGE_EMAIL} element={<ChangeMailview />} />
         <Route path={URL.URL_RESEND_ACTIVATION_TOKEN} element={<ResendActivationTokenview />} />
         <Route path={URL.URL_FORGOTTEN_PASSWORD} element={<ForgottenPasswordview />} />
-        <Route path={URL.URL_ORDER_HISTORIC} element={<OrderHistoricview />} />
+        <Route path={URL.URL_SEARCH} element={<Searchview/>} />
+        {/* <Route path={URL.URL_ORDER_HISTORIC} element={<OrderHistoricview />} /> */}
         <Route path={URL.URL_SINGLE_ORDER_HISTORIC + "/:reference"} element={<SingleOrderHistoricview />} />
     </RoutesContainer>
   )
