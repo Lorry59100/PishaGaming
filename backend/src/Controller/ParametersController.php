467e13ab14d68d3eec5137fa524f6c6e238e17fc
pisha <pisha@hotmail.fr>
2024-08-16 14:29:46 +0200

    [INIT] - change-pseudo, get-user-data, upload-user-image first init + cropAndResizeImage function first init

--- backend/src/Controller/ParametersController.php
+++ backend/src/Controller/ParametersController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class ParametersController extends AbstractController
@@ -129,5 +130,134 @@ class ParametersController extends AbstractController
         } else {
             return new JsonResponse(['error' => 'une erreur est survenue']);
         }
+    }
+
+    /**
+     * @Route("/change-pseudo", name="change_pseudo", methods={"POST"})
+     */
+    public function changePseudo(Request $request, EntityManagerInterface $em) : JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $pseudo = $data['pseudo'];
+        $user = $this->tokenService->getUserFromRequest($request);
+        if ($user) {
+            $user->setPseudo($pseudo);
+            $em->persist($user);
+            $em->flush();
+            // Renvoyer les nouvelles informations de l'utilisateur
+            return new JsonResponse(['message' => 'Pseudo changé']);
+        } else {
+            return new JsonResponse(['error' => 'une erreur est survenue'], 400);
+        }
+    }
+
+
+    /**
+    * @Route("/get-user-data", name="get_user_data", methods={"GET"})
+    */
+    public function getUserData(Request $request): JsonResponse
+    {
+        $user = $this->tokenService->getUserFromRequest($request);
+
+        if ($user) {
+            $userDataArray = [
+                'img' => $user->getImg(),
+                'pseudo' => $user->getPseudo(),
+            ];
+            return new JsonResponse($userDataArray, 200);
+        } else {
+            return new JsonResponse(['error' => 'une erreur est survenue'], 500);
+        }
+    }
+    
+    /**
+     * @Route("/upload-user-image", name="upload_user_image", methods={"POST"})
+     */
+    public function uploadImage(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $file = $request->files->get('img');
+
+        if ($file instanceof UploadedFile) {
+            $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+            $newFilename = $originalFilename . '-' . uniqid() . '.' . $file->guessExtension();
+
+            // Recadrer et redimensionner l'image
+            $resizedImagePath = $this->cropAndResizeImage($file->getPathname(), 150, 150);
+
+            try {
+                // Déplacer l'image redimensionnée vers le répertoire de destination
+                copy($resizedImagePath, $this->getParameter('images_directory') . '/' . $newFilename);
+                unlink($resizedImagePath); // Supprimer l'image temporaire
+            } catch (FileException $e) {
+                return new Response('Erreur lors de l\'upload du fichier.', Response::HTTP_INTERNAL_SERVER_ERROR);
+            }
+
+            // Récupérer l'utilisateur actuellement authentifié
+            $user = $this->tokenService->getUserFromRequest($request);
+
+            if (!$user) {
+                return new Response('Utilisateur non authentifié.', Response::HTTP_UNAUTHORIZED);
+            }
+
+            // Enregistrez le nom du fichier dans la base de données si nécessaire
+            $user->setImg($newFilename);
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return new Response('Fichier uploadé avec succès.', Response::HTTP_OK);
+        }
+
+        return new Response('Aucun fichier uploadé.', Response::HTTP_BAD_REQUEST);
+    }
+
+    private function cropAndResizeImage($imagePath, $width, $height)
+    {
+        list($originalWidth, $originalHeight, $type) = getimagesize($imagePath);
+
+        switch ($type) {
+            case IMAGETYPE_JPEG:
+                $image = imagecreatefromjpeg($imagePath);
+                break;
+            case IMAGETYPE_PNG:
+                $image = imagecreatefrompng($imagePath);
+                break;
+            case IMAGETYPE_GIF:
+                $image = imagecreatefromgif($imagePath);
+                break;
+            default:
+                throw new \Exception('Type de fichier non supporté.');
+        }
+
+        // Calculer les dimensions de recadrage
+        $cropSize = min($originalWidth, $originalHeight);
+        $cropX = ($originalWidth - $cropSize) / 2;
+        $cropY = ($originalHeight - $cropSize) / 2;
+
+        // Créer une image carrée recadrée
+        $croppedImage = imagecreatetruecolor($cropSize, $cropSize);
+        imagecopy($croppedImage, $image, 0, 0, $cropX, $cropY, $cropSize, $cropSize);
+
+        // Redimensionner l'image recadrée
+        $resizedImage = imagecreatetruecolor($width, $height);
+        imagecopyresampled($resizedImage, $croppedImage, 0, 0, 0, 0, $width, $height, $cropSize, $cropSize);
+
+        $tempFilePath = tempnam(sys_get_temp_dir(), 'resized_image');
+        switch ($type) {
+            case IMAGETYPE_JPEG:
+                imagejpeg($resizedImage, $tempFilePath);
+                break;
+            case IMAGETYPE_PNG:
+                imagepng($resizedImage, $tempFilePath);
+                break;
+            case IMAGETYPE_GIF:
+                imagegif($resizedImage, $tempFilePath);
+                break;
+        }
+
+        imagedestroy($image);
+        imagedestroy($croppedImage);
+        imagedestroy($resizedImage);
+
+        return $tempFilePath;
     }
 }
