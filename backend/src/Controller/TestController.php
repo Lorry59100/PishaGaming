3717574184fb77255ea28b9d0d9ac3cbcb1d7c00
pisha <pisha@hotmail.fr>
2024-09-09 11:32:48 +0200

    [INIT] - user-tests, get-test, update-test first init

--- backend/src/Controller/TestController.php
+++ backend/src/Controller/TestController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Test;
 use App\Entity\Point;
+use App\Repository\PointRepository;
 use App\Repository\ProductRepository;
+use App\Repository\TestRepository;
 use App\Service\TokenService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,6 +22,32 @@ class TestController extends AbstractController
         $this->tokenService = $tokenService;
     }
 
+
+    /**
+     * @Route("/user-tests", name="user_tests", methods={"GET"})
+     */
+    public function userTests(Request $request, TestRepository $testRepository, EntityManagerInterface $em): JsonResponse
+    {
+        // Récupérer les données
+        $data = json_decode($request->getContent(), true);
+
+        // Récupérer l'utilisateur
+        $user = $this->tokenService->getUserFromRequest($request);
+
+        $tests = $user->getTests();
+        $testsArray = [];
+        foreach ($tests as $test) {
+            $testsArray[] = [
+                'id' => $test->getId(),
+                'productId' => $test->getProduct()->getid(),
+                'img' => $test->getProduct()->getImg(),
+                'comment' => $test->getComment(),
+                'rate' => $test->getRate(),
+            ];
+        };
+
+        return new JsonResponse($testsArray, 200);
+    }
 
     /**
      * @Route("/submit-test", name="submit_test", methods={"POST"})
@@ -72,5 +100,95 @@ class TestController extends AbstractController
         $em->flush();
 
         return new JsonResponse('kikoo', 200);
+    }
+
+    /**
+     * @Route("/get-test/{id}", name="get_test", methods={"GET"})
+     */
+    public function getTest(Request $request, ProductRepository $productRepository, TestRepository $testRepository, PointRepository $pointRepository, EntityManagerInterface $em, $id): JsonResponse
+    {
+        // Récupérer l'utilisateur
+        $user = $this->tokenService->getUserFromRequest($request);
+
+        // Récupérer le produit en BDD
+        $product = $productRepository->find($id);
+
+        // Récupérer le test correspondant à l'utilisateur et au produit
+        $test = $testRepository->findOneBy(['user' => $user, 'product' => $product]);
+
+        $points = $test->getPoints();
+        $positivePoints = [];
+        $negativePoints = [];
+
+        foreach ($points as $point) {
+            if ($point->isIsPositive() === true) {
+                $positivePoints[] = $point->getDescription();
+            } elseif ($point->isIsPositive() === false) {
+                $negativePoints[] = $point->getDescription();
+            }
+        }
+
+        // Retourner les données du test sous forme de réponse JSON
+    return new JsonResponse([
+        'rate' => $test->getRate(),
+        'comment' => $test->getComment(),
+        'positivePoints' => $positivePoints,
+        'negativePoints' => $negativePoints,
+    ]);
+    }
+
+    /**
+     * @Route("/update-test/{id}", name="update_test", methods={"PUT"})
+     */
+    public function updateTest(Request $request, TestRepository $testRepository, PointRepository $pointRepository, ProductRepository $productRepository, EntityManagerInterface $em, $id): JsonResponse
+    {
+        // Récupérer les données envoyées par le frontend
+        $data = json_decode($request->getContent(), true);
+
+        // Récupérer l'utilisateur
+        $user = $this->tokenService->getUserFromRequest($request);
+
+        // Récupérer le produit en BDD
+        $product = $productRepository->find($id);
+
+        // Récupérer le test correspondant à l'utilisateur et au produit
+        $test = $testRepository->findOneBy(['user' => $user, 'product' => $product]);
+
+        if (!$test) {
+            return new JsonResponse(['error' => 'Test not found'], 404);
+        }
+
+        // Mettre à jour les champs du test
+        $test->setRate($data['rating']);
+        $test->setComment($data['description']);
+
+        // Mettre à jour les points positifs et négatifs
+        $this->updatePoints($test, $data['pros'], true, $pointRepository, $em);
+        $this->updatePoints($test, $data['cons'], false, $pointRepository, $em);
+
+        // Sauvegarder les modifications
+        $em->flush();
+
+        return new JsonResponse(['message' => 'Test updated successfully'], 200);
+    }
+
+    private function updatePoints(Test $test, array $points, bool $isPositive, PointRepository $pointRepository, EntityManagerInterface $em)
+    {
+        // Supprimer les points existants
+        $existingPoints = $pointRepository->findBy(['test' => $test, 'isPositive' => $isPositive]);
+        foreach ($existingPoints as $existingPoint) {
+            $em->remove($existingPoint);
+        }
+
+        // Ajouter les nouveaux points
+        foreach ($points as $description) {
+            if (!empty($description)) {
+                $point = new Point();
+                $point->setDescription($description);
+                $point->setIsPositive($isPositive);
+                $point->setTest($test);
+                $em->persist($point);
+            }
+        }
     }
 }
