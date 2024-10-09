d9f3739cb9ba14055ad6969f3ad96694e4b7739d
pisha <pisha@hotmail.fr>
2024-10-09 14:32:13 +0200

    [FIX] - migration

---
+++ backend/migrations/Version20241001131546.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241001131546 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE side_images (id INT AUTO_INCREMENT NOT NULL, product_id INT DEFAULT NULL, img VARCHAR(255) DEFAULT NULL, INDEX IDX_3C7152444584665A (product_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE side_images ADD CONSTRAINT FK_3C7152444584665A FOREIGN KEY (product_id) REFERENCES product (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE side_images DROP FOREIGN KEY FK_3C7152444584665A');
+        $this->addSql('DROP TABLE side_images');
+    }
+}
