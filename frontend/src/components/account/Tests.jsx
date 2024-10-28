d4dd261b87b0de8a787291eecbd2738544797aef
pisha <pisha@hotmail.fr>
2024-10-28 14:08:25 +0100

    [FIX] - using var instead of direct URLS, console.logs removed

--- frontend/src/components/account/Tests.jsx
+++ frontend/src/components/account/Tests.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import { useTokenService } from "./services/tokenService";
 import axios from "axios";
-import { URL, URL_USER_TESTS } from "../../constants/urls/URLBack";
+import { URL, URL_MAIN_IMG, URL_USER_TESTS } from "../../constants/urls/URLBack";
 import "../../assets/styles/components/tests.css";
 import RatingCircle from "../products/RatingCircle";
 import { PiPencilSimpleLineFill } from "react-icons/pi";
 import { IconContext } from "react-icons";
 import { useNavigate } from 'react-router-dom';
+import { URL_SINGLE_PRODUCT } from "../../constants/urls/URLFront";
 
 
 // Fonction utilitaire pour tronquer le texte
@@ -24,23 +25,20 @@ export function Tests() {
 
     useEffect(() => {
         if (decodedUserToken) {
-          const headers = {
+        const headers = {
             'Authorization': `Bearer ${decodedUserToken.username}`,
             // autres en-têtes si nécessaire...
-          };
+        };
 
-          axios.get(`${URL}${URL_USER_TESTS}`, {headers})
+            axios.get(`${URL}${URL_USER_TESTS}`, {headers})
             .then(response => {
-                console.log(response.data);
                 setTests(response.data);
             })
             .catch(error => {
                 console.error('Erreur lors de la récupération des adresses :', error);
             });
         }
-      }, [decodedUserToken]);
-
-      console.log(tests);
+    }, [decodedUserToken]);
 
     return (
         <div className="user-tests-container">
@@ -48,10 +46,10 @@ export function Tests() {
             <div className="tests-grid">
                 {tests.map(test => (
                     <div key={test.id} className="test-user-item">
-                        <img src={test.img} alt={test.id} />
+                        <img src={`${URL}${URL_MAIN_IMG}/${test.img}`} alt={test.id} />
                         <RatingCircle singleRating={test.rate} />
                         <h5 className="test-desc">{truncateText(test.comment, 200)}</h5>
-                        <button className='submit-button user-test-btn' onClick={() => navigate(`/single-product/${test.productId}`, { state: { editTest: true } })}>
+                        <button className='submit-button user-test-btn' onClick={() => navigate(`${URL_SINGLE_PRODUCT.replace(':id', test.productId)}`, { state: { editTest: true } })}>
                             <h4>Editer mon avis</h4>
                             <IconContext.Provider value={{ size: "1em"}}>
                                 <PiPencilSimpleLineFill/>
