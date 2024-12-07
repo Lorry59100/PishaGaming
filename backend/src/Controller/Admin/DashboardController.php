c7018b8842880c40c6e60f2a20c6b39c2caf0048
pisha . <pisha@hotmail.fr>
2024-12-07 14:22:29 +0100

    [FIX & INIT] - adding logo & link to other admin crud first init

--- backend/src/Controller/Admin/DashboardController.php
+++ backend/src/Controller/Admin/DashboardController.php
@@ -4,6 +4,11 @@ namespace App\Controller\Admin;
 
 use Symfony\Component\HttpFoundation\Response;
 use App\Controller\Admin\ProductCrudController;
+use App\Entity\Cart;
+use App\Entity\Order;
+use App\Entity\Product;
+use App\Entity\Test;
+use App\Entity\User;
 use Symfony\Component\Routing\Annotation\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
@@ -22,11 +27,14 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('PishaGaming');
+            ->setTitle('<img src="images/logo.png" alt="Logo" style="height: 40px;"> PishaGaming');
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Produits', 'fa-solid fa-cube', Product::class);
+        yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-user', User::class);
+        yield MenuItem::linkToCrud('Commandes', 'fa-solid fa-cubes', Order::class);
+        yield MenuItem::linkToCrud('Tests', 'fa-solid fa-clipboard', Test::class);
     }
 }
